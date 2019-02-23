839e710a786c5fe826987090234d2b7406fcff9b
Joel Nordell <joel.nordell@erisx.com>
2019-02-23 16:17:21 -0600

    SIGNFUNC/BCH/LTC/WIP: Add tests for custom signing func for BCH and LTC
      -- refs CO-894

--- test/v2/unit/coins/bch.js
+++ test/v2/unit/coins/bch.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 const Promise = require('bluebird');
 const co = Promise.coroutine;
 
+const bitcoin = require('bitgo-utxo-lib');
 const TestV2BitGo = require('../../../lib/test_bitgo');
 
 describe('BCH:', function() {
@@ -128,6 +129,36 @@ describe('BCH:', function() {
           }
         },
         prv: 'xprv9s21ZrQH143K2fJ91S4BRsupcYrE6mmY96fcX5HkhoTrrwmwjd16Cn87cWinJjByrfpojjx7ezsJLx7TAKLT8m8hM5Kax9YcoxnBeJZ3t2k'
+      });
+      signedTransaction.txHex.should.equal('020000000144dea5cb05425f94976e887ccba5686a9a12a3f49710b021508d3d9cd8de16b801000000b7004830450221009e63ff1c8b0860073bc06bbce84f20568251a31f7a12c0ce300dc024e416f28202200b0dcb4a3b6b2cda1886ea6c020884907efd517d23d97e84fbf411aa65d280dd4100004c695221031cd227e40ad61b4e137109cb2845eb6f5a584ed5c67d9d3135cdaa5045a842ea2103a2e7b54c7b2da0992555353b8e26c6acff4248f4351f08787bf3e2efc94b658321025c2a6cde33c2d73ccf12eecf64c54f08f722c2f073824498950695e9883b141253aeffffffff02e803000000000000116a0f426974476f2070327368207465737440a107000000000017a914d039cb3344294a5a384a5508a006444c420cbc118700000000');
+    }));
+
+    it('should successfully sign a prebuilt transaction using a custom signing function', co(function *() {
+      let pub = 'xpub661MyMwAqRbcF9Nc7TbBo1rZAagiWEVPWKbDKThNG8zqjk76HAKLkaSbTn6dK2dQPfuD7xjicxCZVWvj67fP5nQ9W7QURmoMVAX8m6jZsGp';
+      let prv = 'xprv9s21ZrQH143K2fJ91S4BRsupcYrE6mmY96fcX5HkhoTrrwmwjd16Cn87cWinJjByrfpojjx7ezsJLx7TAKLT8m8hM5Kax9YcoxnBeJZ3t2k';
+
+      let sign = co(function *(path, hash) {
+        var hdPath = bitcoin.hdPath(bitcoin.HDNode.fromBase58(prv));
+        var privKey = hdPath.deriveKey(path);
+        return privKey.sign(hash);
+      });
+
+      const signedTransaction = yield tbch.signTransaction({
+        txPrebuild: {
+          txHex: '010000000144dea5cb05425f94976e887ccba5686a9a12a3f49710b021508d3d9cd8de16b80100000000ffffffff02e803000000000000116a0f426974476f2070327368207465737440a107000000000017a914d039cb3344294a5a384a5508a006444c420cbc118700000000',
+          txInfo: {
+            unspents: [{
+              address: '2NCEDmmKNNnqKvnWw7pE3RLzuFe5aHHVy1X',
+              chain: 0,
+              index: 13,
+              value: 504422,
+              txid: 'b816ded89c3d8d5021b01097f4a3129a6a68a5cb7c886e97945f4205cba5de44',
+              redeemScript: '5221031cd227e40ad61b4e137109cb2845eb6f5a584ed5c67d9d3135cdaa5045a842ea2103a2e7b54c7b2da0992555353b8e26c6acff4248f4351f08787bf3e2efc94b658321025c2a6cde33c2d73ccf12eecf64c54f08f722c2f073824498950695e9883b141253ae'
+            }]
+          }
+        },
+        pub: pub,
+        sign: sign
       });
       signedTransaction.txHex.should.equal('020000000144dea5cb05425f94976e887ccba5686a9a12a3f49710b021508d3d9cd8de16b801000000b7004830450221009e63ff1c8b0860073bc06bbce84f20568251a31f7a12c0ce300dc024e416f28202200b0dcb4a3b6b2cda1886ea6c020884907efd517d23d97e84fbf411aa65d280dd4100004c695221031cd227e40ad61b4e137109cb2845eb6f5a584ed5c67d9d3135cdaa5045a842ea2103a2e7b54c7b2da0992555353b8e26c6acff4248f4351f08787bf3e2efc94b658321025c2a6cde33c2d73ccf12eecf64c54f08f722c2f073824498950695e9883b141253aeffffffff02e803000000000000116a0f426974476f2070327368207465737440a107000000000017a914d039cb3344294a5a384a5508a006444c420cbc118700000000');
     }));
